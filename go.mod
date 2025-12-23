623216f918930e8abd4fecc8c6fadd4eec24c329
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-12-23 17:22:37 +0300

    add sentry

--- go.mod
+++ go.mod
@@ -2,7 +2,12 @@ module shorty
 
 go 1.25.5
 
-require github.com/gin-gonic/gin v1.11.0
+require (
+	github.com/getsentry/sentry-go v0.40.0
+	github.com/getsentry/sentry-go/gin v0.40.0
+	github.com/gin-gonic/gin v1.11.0
+	github.com/sirupsen/logrus v1.9.3
+)
 
 require (
 	github.com/bytedance/sonic v1.14.0 // indirect
@@ -19,7 +24,7 @@ require (
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/quic-go/qpack v0.5.1 // indirect
--- main.go
+++ main.go
@@ -1,26 +1,73 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"errors"
+	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func setupRouter() *gin.Engine {
-	router := gin.Default()
+	router := gin.New()
+
 	router.Use(gin.Logger())
+
+	router.Use(sentrygin.New(sentrygin.Options{
+		Repanic:         true,
+		WaitForDelivery: false,
+		Timeout:         2 * time.Second,
+	}))
+
 	router.Use(gin.Recovery())
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
 
+	router.GET("/debug/sentry", func(c *gin.Context) {
+		err := errors.New("test error from /debug/sentry")
+
+		if hub := sentrygin.GetHubFromContext(c); hub != nil {
+			hub.CaptureException(err)
+		} else {
+			sentry.CaptureException(err)
+		}
+
+		c.String(http.StatusInternalServerError, "sent to sentry")
+	})
+
 	return router
 }
-func main() {
-	router := setupRouter()
 
-	err := router.Run(":8080")
-	if err != nil {
+func initSentry() {
+	dsn := os.Getenv("SENTRY_DSN")
+	if dsn == "" {
+		log.Println("SENTRY_DSN is empty, sentry disabled")
 		return
 	}
+
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn: dsn,
+	}); err != nil {
+		log.Printf("sentry init failed: %v", err)
+	}
+}
+
+func main() {
+	initSentry()
+	defer sentry.Flush(2 * time.Second)
+
+	router := setupRouter()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	_ = router.Run(":" + port)
 }
